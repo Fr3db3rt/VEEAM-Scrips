b0c95fa466733858d748890937b481ee11f448d8
Fr3db3rt <fr3db3rt@gmail.com>
2017-03-07 14:54:52 +0100

    Update Veeam_Tape_nach_Media_Pool.cmd

--- Veeam_Tape_nach_Media_Pool.cmd
+++ Veeam_Tape_nach_Media_Pool.cmd
@@ -1,7 +1,10 @@
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'C:\Scripts\Veeam_Tape_nach_Media_Pool.ps1'"
+rem PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'C:\Scripts\Veeam_Tape_nach_Media_Pool.ps1'"
+
+@start "Start-Tape-Job" cmd /c PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {(Add-PSSnapin VeeamPSSnapin); (Get-VBRLocalhost | Get-VBRTapeServer); (Get-VBRTapeServer | Get-VBRTapeLibrary); (Get-VBRTapeLibrary | Start-VBRTapeInventory -wait); ($drive = Get-VBRTapeDrive); (Get-VBRTapeMedium -Drive $drive | Start-VBRTapeCatalog -Wait); (Get-VBRTapeMedium -Drive $drive | Move-VBRTapeMedium -MediaPool "Free" -Confirm:$false); (Get-VBRTapeMedium -Drive $drive | Erase-VBRTapeMedium -wait -Confirm:$false)}" 
+
+goto :end
 
 --------------------------------
-
 Add-PSSnapin VeeamPSSnapin
 
 # B&R Server ermitteln und anschliessend Tape Server ermitteln
@@ -16,16 +19,22 @@ Get-VBRTapeLibrary | Start-VBRTapeInventory -wait
 # Drive identifizieren und aktuelles Tape identifizieren 
 $drive = Get-VBRTapeDrive
 Get-VBRTapeMedium -Drive $drive
-####
 
-# Drive identifizieren und aktuelles Tape identifizieren und auf Abschluss warten
-$tape = Get-VBRTapeDrive | Get-VBRTapeMedium
+##### Drive identifizieren und aktuelles Tape identifizieren und auf Abschluss warten
+###$tape = Get-VBRTapeDrive | Get-VBRTapeMedium
 
 # Katalog des aktuellen Bandes einlesen und auf Abschluss warten
 Start-VBRTapeCatalog -Medium $tape -wait
+Get-VBRTapeMedium -Drive $drive | Start-VBRTapeCatalog -wait
 
 # aktuelles Medium in den Pool 'FREE' schieben
 Move-VBRTapeMedium -Medium $tape -MediaPool "Free" -Confirm:$false
+Get-VBRTapeMedium -Drive $drive | Move-VBRTapeMedium -MediaPool "Free" -Confirm:$false
 
 # aktuelles Medium loeschen und auf Abschluss warten
 Erase-VBRTapeMedium -Medium $tape -wait -Confirm:$false
+Get-VBRTapeMedium -Drive $drive | Erase-VBRTapeMedium -wait -Confirm:$false
+
+:end
+
+
