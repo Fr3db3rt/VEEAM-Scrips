8f71701bb9e7c1b462e7c3a40cddff019e827121
Fr3db3rt <fr3db3rt@gmail.com>
2017-03-07 16:45:26 +0100

    Delete Start-Tape-Job.cmd

--- Start-Tape-Job.cmd
+++
@@ -1,4 +0,0 @@
-: Start-Tape-Job.cmd
-
-@start "Start-Tape-Job" cmd /c PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {(Add-PSSnapin VeeamPSSnapin); (Get-VBRTapeJob | Start-VBRJob -FullBackup)}"
-
